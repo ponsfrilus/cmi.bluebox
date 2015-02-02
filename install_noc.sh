cdd1256cb36e322ea6b430399774ad9237382c64
Dominique Quatravaux <dominique@quatravaux.org>
2015-02-02 17:16:56 +0100

    Add a symlink – We'll see what happens.

--- install_noc.sh
+++ install_noc.sh
@@ -6,3 +6,8 @@ which puppet || {
     echo >&2 Please install puppet first.
     exit 1
 }
+
+: ${CHECKOUT_DIR:="${dirname "$0"}"}
+: ${PUPPET_MODULES_DIR:=/etc/puppet/environments/production/modules}
+
+ln -sf "${CHECKOUT_DIR}"/noc/puppet/modules/blueboxnoc "$PUPPET_MODULES_DIR"/
